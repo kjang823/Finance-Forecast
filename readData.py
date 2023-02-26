565791adf4368b802c96ee7dc4c194657ce2ad10
Kevin Jang <tul56945@temple.edu>
2023-02-26 14:25:58 -0500

    Finished base implementation for API calls of all (excluding premium API features)

--- readData.py
+++ readData.py
@@ -11,7 +11,10 @@ NOTE: There are no API calls made for Premium features. The list of these featur
         TIME_SERIES_DAILY
         FOREX_INTRADAY
         CRYPTOCURRENCIES_INTRADAY
-        
+        TECHNICAL_INDICATORS_STOCHASTIC_OSCILLATOR
+        TECHNICAL_INDICATORS_RELATIVE_STRENGTH_INDEX
+        TECHNICAL_INDICATORS_AVERAGE_DIRECTIONAL_MOVEMENT_INDEX
+        TECHNICAL_INDICATORS_COMMODITY_CHANNEL_INDEX_VALUES
 """
 
 api_File = open('APIKEY', 'r')
@@ -455,3 +458,257 @@ class technical_Indicators:
         r = requests.get(url)
         data = r.json()
         return data
+
+    def moving_Average_Convergence_Divergence(self):
+        url = 'https://www.alphavantage.co/query?function=MACD&symbol=IBM&interval=daily&series_type=open&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def moving_Average_Converge_Diverge_Control_Average(self):
+        url = 'https://www.alphavantage.co/query?function=MACDEXT&symbol=IBM&interval=daily&series_type=open&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def stochastic_Fast(self):
+        url = 'https://www.alphavantage.co/query?function=STOCHF&symbol=IBM&interval=daily&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def stochastic_Relative_Strength_Index(self):
+        url = 'https://www.alphavantage.co/query?function=STOCHRSI&symbol=IBM&interval=daily&time_period=10&series_type=close&fastkperiod=6&fastdmatype=1&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def williams_Percentage(self):
+        url = 'https://www.alphavantage.co/query?function=WILLR&symbol=IBM&interval=daily&time_period=10&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def average_Directional_Movement_Index_Rating(self):
+        url = 'https://www.alphavantage.co/query?function=ADXR&symbol=IBM&interval=daily&time_period=10&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def absolute_Price_Oscillator(self):
+        url = 'https://www.alphavantage.co/query?function=APO&symbol=IBM&interval=daily&series_type=close&fastperiod=10&matype=1&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def percentage_Price_Oscillator(self):
+        url = 'https://www.alphavantage.co/query?function=PPO&symbol=IBM&interval=daily&series_type=close&fastperiod=10&matype=1&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def momentum(self):
+        url = 'https://www.alphavantage.co/query?function=MOM&symbol=IBM&interval=daily&time_period=10&series_type=close&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def balance_Of_Power(self):
+        url = 'https://www.alphavantage.co/query?function=BOP&symbol=IBM&interval=daily&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def chande_Momentum_Oscillator(self):
+        url = 'https://www.alphavantage.co/query?function=CMO&symbol=IBM&interval=weekly&time_period=10&series_type=close&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def rate_Of_Change(self):
+        url = 'https://www.alphavantage.co/query?function=ROC&symbol=IBM&interval=weekly&time_period=10&series_type=close&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def rate_Of_Change_Ratio(self):
+        url = 'https://www.alphavantage.co/query?function=ROCR&symbol=IBM&interval=daily&time_period=10&series_type=close&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def aroon(self):
+        url = 'https://www.alphavantage.co/query?function=AROON&symbol=IBM&interval=daily&time_period=14&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def aroon_Oscillator(self):
+        url = 'https://www.alphavantage.co/query?function=AROONOSC&symbol=IBM&interval=daily&time_period=10&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def money_Flow_Index(self):
+        url = 'https://www.alphavantage.co/query?function=MFI&symbol=IBM&interval=weekly&time_period=10&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def triple_Smooth_Exponential_Moving_Average(self):
+        url = 'https://www.alphavantage.co/query?function=TRIX&symbol=IBM&interval=daily&time_period=10&series_type=close&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def ultimate_Oscillator(self):
+        url = 'https://www.alphavantage.co/query?function=ULTOSC&symbol=IBM&interval=daily&timeperiod1=8&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def directional_Movement_Index(self):
+        url = 'https://www.alphavantage.co/query?function=DX&symbol=IBM&interval=daily&time_period=10&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def minus_Directional_Indicator(self):
+        url = 'https://www.alphavantage.co/query?function=MINUS_DI&symbol=IBM&interval=weekly&time_period=10&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def plus_Directional_Indicator(self):
+        url = 'https://www.alphavantage.co/query?function=PLUS_DI&symbol=IBM&interval=daily&time_period=10&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def bollinger_Brands(self):
+        url = 'https://www.alphavantage.co/query?function=BBANDS&symbol=IBM&interval=weekly&time_period=5&series_type=close&nbdevup=3&nbdevdn=3&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def midpoint(self):
+        url = 'https://www.alphavantage.co/query?function=MIDPOINT&symbol=IBM&interval=daily&time_period=10&series_type=close&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def midprice(self):
+        url = 'https://www.alphavantage.co/query?function=MIDPRICE&symbol=IBM&interval=daily&time_period=10&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def parabolic_Stop_And_Reverse(self):
+        url = 'https://www.alphavantage.co/query?function=SAR&symbol=IBM&interval=weekly&acceleration=0.05&maximum=0.25&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def true_Range(self):
+        url = 'https://www.alphavantage.co/query?function=TRANGE&symbol=IBM&interval=daily&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def average_True_Range(self):
+        url = 'https://www.alphavantage.co/query?function=ATR&symbol=IBM&interval=daily&time_period=14&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def normalized_Average_True_Range(self):
+        url = 'https://www.alphavantage.co/query?function=NATR&symbol=IBM&interval=weekly&time_period=14&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def chaikin_Accumulation_Distribution_Line(self):
+        url = 'https://www.alphavantage.co/query?function=AD&symbol=IBM&interval=daily&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def chaikin_Accumulation_Distribution_Oscillator(self):
+        url = 'https://www.alphavantage.co/query?function=ADOSC&symbol=IBM&interval=daily&fastperiod=5&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def onbalance_Volume(self):
+        url = 'https://www.alphavantage.co/query?function=OBV&symbol=IBM&interval=weekly&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def hilbert_Transform_Instantaneous_Trendline(self):
+        url = 'https://www.alphavantage.co/query?function=HT_TRENDLINE&symbol=IBM&interval=daily&series_type=close&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def hilbert_Transform_Sine_Wave(self):
+        url = 'https://www.alphavantage.co/query?function=HT_SINE&symbol=IBM&interval=daily&series_type=close&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def hilbert_Transform_Trend_Cycle(self):
+        url = 'https://www.alphavantage.co/query?function=HT_TRENDMODE&symbol=IBM&interval=weekly&series_type=close&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def hilbert_Transform_Dominant_Cycle(self):
+        url = 'https://www.alphavantage.co/query?function=HT_DCPERIOD&symbol=IBM&interval=daily&series_type=close&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+    def hilbert_Transform_Phasor_Componenets(self):
+        url = 'https://www.alphavantage.co/query?function=HT_PHASOR&symbol=IBM&interval=weekly&series_type=close&apikey=demo'
+        url = url.replace("demo", api_String)
+        r = requests.get(url)
+        data = r.json()
+        return data
+
+
