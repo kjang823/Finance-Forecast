a8eb9721751264f263e3c2a503298abfbfe30e88
Kevin Jang <tul56945@temple.edu>
2023-02-26 01:26:01 -0500

    Updated to within Forex base implementation. Stopped at Intraday. Need to add in premium features not utilized within documentation portion at top.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import readData
 
 
 def print_hi(name):
@@ -11,6 +12,8 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # print_hi('PyCharm')
+    print(readData.time_Series_Intraday())
+    print(readData.time_Series_Intraday_Extended())
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
